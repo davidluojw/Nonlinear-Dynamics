413cf62c86c2cd57d384554faf9e4b8579effab0
lawkawai <ddlljiawei@163.com>
2025-02-21 18:19:04 +0800

    pendulum trajectories and angle angle velocity to time

--- pendulum.m
+++ pendulum.m
@@ -8,9 +8,11 @@ axis equal
 axis([0, 4*pi, -3, 3])
 set(gca, 'XTick', (0:4)*pi)
 set(gca, 'XTickLabel', {'0', 'pi', '2pi', '3pi', '4pi'})
-
-
-figure;
+hold on;
+% hold off;
+% 
+% 
+% figure;
 colors = 'rgbyc';
 rhs = @(t, x) [x(2); -0.1*x(2) - sin(x(1))];
 options= odeset('RelTol', 1e-7);
@@ -26,3 +28,23 @@ xlabel 'x'
 ylabel 'y'
 set(gca, 'XTick', (0:4)*pi)
 set(gca, 'XTickLabel', {'0', 'pi', '2pi', '3pi', '4pi'})
+hold off;
+
+
+figure, hold on
+for y0 = 0:0.2:3
+    [t, x] = ode45(rhs, [0, 25], [0; y0], options);
+    subplot(2, 1, 1), hold on
+    plot(t, x(:,1))
+    subplot(2, 1, 2), hold on
+    plot(t, x(:, 2))
+    hold on
+end
+subplot(2, 1, 1)
+xlabel t
+ylabel 'x = angle'
+set(gca, 'YTick', (0:4)*pi)
+set(gca, 'YTickLabel', {'0', 'pi', '2pi', '3pi', '4pi'})
+subplot(2, 1, 2)
+xlabel t
+ylabel 'y = angular velocity'
